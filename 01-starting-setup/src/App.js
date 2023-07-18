a622dbc8770bbe6f659b4a82e72f5008a5030c8f
Samuel Dourado <sd7038@gmail.com>
2023-07-18 18:38:54 -0300

    adiciona formatação de data no ExpenseItem

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,7 +1,45 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
+  const expense = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 12.5,
+      date: new Date(2023, 7, 17),
+    },
+    {
+      id: "e2",
+      title: "TV",
+      amount: 700,
+      date: new Date(2023, 7, 10),
+    },
+    {
+      id: "e3",
+      title: "New Desk",
+      amount: 400,
+      date: new Date(2023, 7, 20),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expense[0].title}
+        amount={expense[0].amount}
+        date={expense[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[1].title}
+        amount={expense[1].amount}
+        date={expense[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[2].title}
+        amount={expense[2].amount}
+        date={expense[2].date}
+      ></ExpenseItem>
     </div>
   );
 }
---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,27 @@
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+    const expenseDate = props.date;
+    const expenseTitle = props.title;
+    const expenseAmount = props.amount;
+
+    const mouth = expenseDate.toLocaleString('en-US', {month: 'long'});
+    const day = expenseDate.toLocaleString('en-US', {day: '2-digit'});
+    const year = expenseDate.getFullYear();
+
+    return (
+        <div className='expense-item'>
+            <div>
+                <div>{mouth}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
+            <div className='expense-item__description'>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>${expenseAmount}</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
