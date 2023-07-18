be13974d7e7f2a7de566b2f924503ca51d3b5624
Samuel Dourado <sd7038@gmail.com>
2023-07-18 18:57:18 -0300

    Adiciona o componente ExpenseDate para mostrar datas

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,21 +1,14 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
     const expenseDate = props.date;
     const expenseTitle = props.title;
     const expenseAmount = props.amount;
 
-    const mouth = expenseDate.toLocaleString('en-US', {month: 'long'});
-    const day = expenseDate.toLocaleString('en-US', {day: '2-digit'});
-    const year = expenseDate.getFullYear();
-
     return (
         <div className='expense-item'>
-            <div>
-                <div>{mouth}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date={expenseDate}></ExpenseDate>
             <div className='expense-item__description'>
                 <h2>{expenseTitle}</h2>
                 <div className='expense-item__price'>${expenseAmount}</div>
